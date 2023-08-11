87a38be09a8be9f052c4eecd4ae7a54d3cd2dacb
v_nikeyang <1013195908@qq.com>
2023-08-11 16:38:09 +0800

    update dml.sql

--- db/upgrade/0.3.2-webnak/upgrade_dml.sql
+++ db/upgrade/0.3.2-webnak/upgrade_dml.sql
@@ -8,4 +8,16 @@ INSERT INTO linkis_ps_error_code
 VALUES('43044', 'Hadoop认证失败，请联系管理员处理', 'Hadoop securith with Kerberos is enabled but the login user does not have Kerberos credentials or delegation tokens', 0);
 INSERT INTO linkis_ps_error_code
 (error_code, error_desc, error_regex, error_type)
-VALUES( '43045', '缓存问题，请尝试重启', 'EngineConnServer Start Failed.java.lang.NoClassDefFoundError', 0);
+VALUES('43045', '缓存问题，请尝试重启', 'EngineConnServer Start Failed.java.lang.NoClassDefFoundError', 0);
+INSERT INTO linkis_ps_error_code
+(error_code, error_desc, error_regex, error_type)
+VALUES('43046', 'hdfs资源文件下载失败，请尝试重启任务或者重新上传资源文件', 'Could not obtain block', 0);
+INSERT INTO linkis_ps_error_code
+(error_code, error_desc, error_regex, error_type)
+VALUES('43047', 'ims 连接失败', 'Connection timed out (\\S+) at com.webank.ims.common', 0);
+INSERT INTO linkis_ps_error_code
+(error_code, error_desc, error_regex, error_type)
+VALUES('43048', '缺少字段: %s', 'Object (\\S+) not found', 0);
+INSERT INTO linkis_ps_error_code
+(error_code, error_desc, error_regex, error_type)
+VALUES('43049', 'topic没有权限', 'Not authorized to access topics', 0);
