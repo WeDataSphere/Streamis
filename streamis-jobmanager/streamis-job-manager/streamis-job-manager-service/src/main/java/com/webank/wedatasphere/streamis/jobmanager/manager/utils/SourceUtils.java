0ee9c381f0b4702ba726f7f5769fd936c1d51f40
v_nikeyang <1013195908@qq.com>
2023-10-20 17:21:28 +0800

    fix code review

--- streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/java/com/webank/wedatasphere/streamis/jobmanager/manager/utils/SourceUtils.java
+++ streamis-jobmanager/streamis-job-manager/streamis-job-manager-service/src/main/java/com/webank/wedatasphere/streamis/jobmanager/manager/utils/SourceUtils.java
@@ -8,6 +8,7 @@ import org.apache.linkis.server.BDPJettyServerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -15,46 +16,44 @@ public class SourceUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(SourceUtils.class);
 
-    public static JobHighAvailableVo manageJobProjectFile(String highAvailablePolicy,String source) {
+    public static JobHighAvailableVo manageJobProjectFile(String highAvailablePolicy, String source) {
         highAvailablePolicy = Optional.ofNullable(highAvailablePolicy).orElse(JobConf.HIGHAVAILABLE_DEFAULT_POLICY().getHotValue());
         JobHighAvailableVo highAvailableVo = new JobHighAvailableVo();
-        try {
-            if (!Boolean.parseBoolean(JobConf.HIGHAVAILABLE_ENABLE().getValue().toString())) {
-                highAvailableVo.setHighAvailable(true);
-                highAvailableVo.setMsg("管理员未开启高可用配置");
-                return highAvailableVo;
-            } else {
-                //查job conf  wds.streamis.app.highavailable.policy  值
-                if (highAvailablePolicy.equals(JobConf.HIGHAVAILABLE_POLICY().getValue())){
-                    Map map = BDPJettyServerHelper.gson().fromJson(source, Map.class);
-                    if (map.containsKey("source")) {
-                        String sourceValue = map.get("source").toString();
-                        if (sourceValue.equals(JobConf.HIGHAVAILABLE_SOURCE().getValue())) {
-                            if (map.containsKey("isHighAvailable")) {
-                                highAvailableVo.setHighAvailable(Boolean.parseBoolean(map.get("isHighAvailable").toString()) );
-                            }
-                            highAvailableVo.setMsg(map.getOrDefault("highAvailableMessage","高可用信息为空，请联系管理员").toString());
-                            return highAvailableVo;
-                        }  else {
-                            highAvailableVo.setHighAvailable(true);
-                            highAvailableVo.setMsg("非标准来源,不检测高可用");
-                            return highAvailableVo;
+        if (!Boolean.parseBoolean(JobConf.HIGHAVAILABLE_ENABLE().getValue().toString())) {
+            highAvailableVo.setHighAvailable(true);
+            highAvailableVo.setMsg("管理员未开启高可用配置");
+            return highAvailableVo;
+        } else {
+            if (highAvailablePolicy.equals(JobConf.HIGHAVAILABLE_POLICY().getValue())) {
+                Map map = new HashMap<>();
+                try {
+                    map = BDPJettyServerHelper.gson().fromJson(source, Map.class);
+                } catch (JsonSyntaxException e) {
+                    LOG.error("Failed to source parse JSON");
+                }
+                if (map.containsKey("source")) {
+                    String sourceValue = map.get("source").toString();
+                    if (sourceValue.equals(JobConf.HIGHAVAILABLE_SOURCE().getValue())) {
+                        if (map.containsKey("isHighAvailable")) {
+                            highAvailableVo.setHighAvailable(Boolean.parseBoolean(map.get("isHighAvailable").toString()));
                         }
-                    }   else {
-                        highAvailableVo.setHighAvailable(true);
-                        highAvailableVo.setMsg("非标准来源,不检测高可用");
+                        highAvailableVo.setMsg(map.getOrDefault("highAvailableMessage", "高可用信息为空，请联系管理员").toString());
+                        return highAvailableVo;
+                    } else {
+                        highAvailableVo.setHighAvailable(false);
+                        highAvailableVo.setMsg("非标准来源,不检测高可用,source: " +source);
                         return highAvailableVo;
                     }
                 } else {
                     highAvailableVo.setHighAvailable(true);
-                    highAvailableVo.setMsg("任务未开启高可用");
+                    highAvailableVo.setMsg("不存在来源信息，无法检测高可用");
                     return highAvailableVo;
                 }
+            } else {
+                highAvailableVo.setHighAvailable(true);
+                highAvailableVo.setMsg("任务未开启高可用");
+                return highAvailableVo;
             }
-        } catch (JsonSyntaxException e) {
-            // 转换失败，处理异常情况
-            LOG.error("Failed to source parse JSON");
         }
-        return highAvailableVo;
     }
 }
