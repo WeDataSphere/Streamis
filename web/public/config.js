0bb2dfaecd9a3262322276e76d165530ae14745d
bert_cai <bert_cai@outlook.com>
2024-06-17 15:27:21 +0800

    upd: config改为发布时在发布模板替换

--- web/public/config.js
+++ web/public/config.js
@@ -3,8 +3,8 @@
  * kn.dss.weoa.com 测试
  * kn.dss.webank.com 生产
  */
-window.knowledgeLibraryUrl = "http://kn.dss.weoa.com/"
+window.knowledgeLibraryUrl = [#knowledgeLibraryUrl]
 // 是否允许上传、配置、arge参数编辑 true 测试 false 生产
-window.enableUpload = true
+window.enableUpload = [#enableUpload]
 // 水印中region字段的设置
-window.watermarkRegion = "东莞"
+window.watermarkRegion = [#watermarkRegion]
